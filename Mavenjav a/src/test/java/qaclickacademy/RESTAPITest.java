cef8442a076415d2841f4d91d704f77a09406530
Shalini Baronia <shalinibaronia@gmail.com>
2022-06-02 12:19:06 -0700

    this is commit from Asian time zone guy

---
+++ Mavenjav a/src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,23 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+		
+	}
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+
+}
